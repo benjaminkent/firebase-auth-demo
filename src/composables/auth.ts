e996ed8b4b9fdf98aef54f026e8879c53d1abca3
benjaminkent <benjamin.k.jehl@gmail.com>
2020-03-16 23:22:40 -0400

    composition rewritten CORRECTLY, everything working as expected

--- src/composables/auth.ts
+++ src/composables/auth.ts
@@ -1,7 +1,7 @@
-import { ref, reactive } from '@vue/composition-api'
-import { User } from 'firebase'
+import { ref, reactive, computed, onMounted } from '@vue/composition-api'
+import firebase, { User } from 'firebase'
 
-export function useAuth() {
+export const useAuth = () => {
   // because i don't want to list
   // all 31 keys for user
   // eslint-disable-next-line
@@ -9,26 +9,46 @@ export function useAuth() {
   // eslint-disable-next-line
   let userToken: any = reactive({})
   const isLoggedIn = ref(false)
+  const loading = ref(false)
 
-  function setUser(userObj: User | null) {
-    user = userObj
+  const showLoading = computed(() => loading.value)
+  const showLoggedInStatus = computed(() => isLoggedIn.value)
+  const showUser = computed(() => user)
+  const showUserToken = computed(() => userToken)
+
+  const setAuthToken = async () => {
+    const currentUser = await firebase.auth().currentUser
+    if (currentUser) {
+      currentUser
+        .getIdToken(/* forceRefresh */ true)
+        .then(token => (userToken = token))
+        .catch(error => console.error(error))
+    }
   }
 
-  // eslint-disable-next-line
-  function setUserToken(token: any) {
-    userToken = token
+  const checkAuthState = async () => {
+    loading.value = true
+    await firebase.auth().onAuthStateChanged(async user => {
+      if (user) {
+        isLoggedIn.value = true
+        user = await firebase.auth().currentUser
+        setAuthToken()
+        loading.value = false
+      } else {
+        isLoggedIn.value = false
+        loading.value = false
+      }
+    })
   }
 
-  function setLoggedInStatus(status: boolean) {
-    isLoggedIn.value = status
-  }
+  onMounted(() => {
+    checkAuthState()
+  })
 
   return {
-    user,
-    userToken,
-    isLoggedIn,
-    setUser,
-    setUserToken,
-    setLoggedInStatus,
+    showLoading,
+    showLoggedInStatus,
+    showUser,
+    showUserToken,
   }
 }
--- src/views/CompositionAuthLayout.vue
+++ src/views/CompositionAuthLayout.vue
@@ -1,59 +1,32 @@
 <template>
-  <div v-if="!loading" class="auth-layout">
-    <app-header :title="'Composition API Auth'" :auth-status="authStatus" />
-    <h2>{{ authStatus ? 'Logged In' : 'Logged Out' }}</h2>
+  <div v-if="!showLoading" class="auth-layout">
+    <app-header :title="'Composition API Auth'" :auth-status="showLoggedInStatus" />
+    <h2>{{ showLoggedInStatus ? 'Logged In' : 'Logged Out' }}</h2>
     <composition-auth />
     <router-link to="/" class="link">Go Home</router-link>
   </div>
 </template>
 
 <script lang="ts">
-import { ref, onMounted } from '@vue/composition-api'
 import AppHeader from '@/components/AppHeader.vue'
 import CompositionAuth from '@/components/CompositionAuth.vue'
 import { useAuth } from '@/composables/auth'
-import firebase from 'firebase'
 
 export default {
   components: { CompositionAuth, AppHeader },
   setup() {
-    const loading = ref(false)
-    const authStatus = ref(false)
-
-    async function getAuthToken() {
-      const currentUser = await firebase.auth().currentUser
-      if (currentUser) {
-        currentUser
-          .getIdToken(/* forceRefresh */ true)
-          .then(token => useAuth().setUserToken(token))
-          .catch(error => console.error(error))
-      }
-    }
-
-    async function checkAuthState() {
-      await firebase.auth().onAuthStateChanged(async user => {
-        if (user) {
-          await useAuth().setLoggedInStatus(true)
-          await useAuth().setUser(firebase.auth().currentUser)
-          await getAuthToken()
-        } else {
-          useAuth().setLoggedInStatus(false)
-          loading.value = false
-        }
-      })
-    }
-
-    onMounted(async () => {
-      loading.value = true
-      await checkAuthState()
-      loading.value = false
-    })
+    const {
+      showLoading,
+      showLoggedInStatus,
+      showUser,
+      showUserToken,
+    } = useAuth()
 
     return {
-      loading,
-      authStatus,
-      checkAuthState,
-      useAuth,
+      showLoading,
+      showLoggedInStatus,
+      showUser,
+      showUserToken,
     }
   },
 }
